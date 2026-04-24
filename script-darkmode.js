8b1386a78ca3514d498d1a4e27f30a360e95c459
paokiwi139-source <paokiwi139@gmail.com>
2026-04-24 21:22:20 +1200

    -changed layout in texts in DFMV.html and Animation-showreel.html. -added 4 more images in comics.html. -added animation for profile image

--- script-darkmode.js
+++ script-darkmode.js
@@ -34,8 +34,39 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
 function toggleSection(id) {
   const section = document.getElementById(id);
   section.style.display = 
     section.style.display === "block" ? "none" : "block";
 }
+document.body.classList.toggle("light-mode");
+
+const profile = document.getElementById("profile-wrapper");
+const sparkles = document.querySelectorAll(".sparkle");
+
+profile.addEventListener("click", () => {
+
+    // RANDOMIZE EACH SPARKLE
+    sparkles.forEach(sparkle => {
+        const angle = Math.random() * 2 * Math.PI; // full circle
+        const distance = 60 + Math.random() * 60;  // how far it travels
+
+        const scale = 0.8 + Math.random() * 0.8;
+sparkle.style.fontSize = `${16 * scale}px`;
+
+        const x = Math.cos(angle) * distance;
+        const y = Math.sin(angle) * distance;
+
+        sparkle.style.setProperty("--x", `${x}px`);
+        sparkle.style.setProperty("--y", `${y}px`);
+        sparkle.style.animationDelay = `${Math.random() * 0.2}s`;
+    });
+
+    // TRIGGER ANIMATION
+    profile.classList.add("active");
+
+    setTimeout(() => {
+        profile.classList.remove("active");
+    }, 900);
+});
